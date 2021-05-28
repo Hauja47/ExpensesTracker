6c68d9f6b3122c9162529f1cd460fceaf2b5d755
Hauja47 <hoangletrunghau01@gmail.com>
2021-05-29 00:15:18 +0700

    Add icon for AddTrasaction screen

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -2,10 +2,16 @@ export const down_arrow = require('../assets/icons/diagonal_arrow_down_right.png
 export const up_arrow = require('../assets/icons/diagonal_arrow_up_right.png')
 export const drop_down_arrow = require('../assets/icons/drop_down_arrow.png')
 export const cancel_icon = require('../assets/icons/cancel.png')
+export const calendar = require('../assets/icons/calendar.png')
+export const wallet = require('../assets/icons/wallet.png')
+export const paragrapgh = require('../assets/icons/edit.png')
 
 export default {
     down_arrow,
     up_arrow,
     drop_down_arrow,
-    cancel_icon
+    cancel_icon,
+    calendar,
+    wallet,
+    paragrapgh
 }
