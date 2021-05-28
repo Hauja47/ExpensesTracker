38190e209e13214ea0ebdf5ca3cbb143595a80b7
Hauja47 <hoangletrunghau01@gmail.com>
2021-05-29 00:17:35 +0700

    Add semi-colon and comma

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import categoriesReducer from './reducers/categoriesReducer';
-import transactionsReducer from './reducers/transactionsReducer'
+import transactionsReducer from './reducers/transactionsReducer';
 
 const rootReducer = combineReducers({
     categoriesReducer,
-    transactionsReducer
+    transactionsReducer,
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
