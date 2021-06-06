118ec63c029ce31b8537a4d3e3f6692a4cb3fa42
Hauja47 <hoangletrunghau01@gmail.com>
2021-06-06 16:18:50 +0700

    Add SplashScreen

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import 'react-native-gesture-handler';
 
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { useDispatch } from 'react-redux';
-
+import { createStackNavigator } from '@react-navigation/stack'
+import { useSelector, useDispatch } from 'react-redux';
 import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
 
+import { store } from './src/redux/store';
 import {
   HomeScreen,
   TransactionDetail,
@@ -15,37 +14,60 @@ import {
   AddCategory,
   SplashScreen
 } from './src/screens/';
+import { getCategories, getTransactions } from './src/redux/actions'
 
 const Stack = createStackNavigator();
 
-const App = () => {
+const AppWapper = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="SplashScreen"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen
-            name="SplashScreen"
-            component={SplashScreen}
-          />
-          <Stack.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-          />
-          <Stack.Screen
-            name="AddTransaction"
-            component={AddTransaction}
-          />
-          <Stack.Screen
-            name="AddCategory"
-            component={AddCategory}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <App />
     </Provider>
+  )
+}
+
+const App = () => {
+
+  const dispatch = useDispatch();
+  const { isTransactionsLoaded } = useSelector(state => state.transactionsReducer)
+  const { isCategoriesLoaded } = useSelector(state => state.categoriesReducer)
+
+  React.useEffect(() => {
+    dispatch(getTransactions());
+    dispatch(getCategories());
+  });
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+      >
+        {(!(isTransactionsLoaded && isCategoriesLoaded)) ?
+          (
+            <Stack.Screen
+              name="SplashScreen"
+              component={SplashScreen}
+            />
+          ) : (
+            <>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+              />
+              <Stack.Screen
+                name="AddTransaction"
+                component={AddTransaction}
+              />
+              <Stack.Screen
+                name="AddCategory"
+                component={AddCategory}
+              />
+            </>
+          )
+        }
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-export default App;
+export default AppWapper;
--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,33 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
     Text,
     SafeAreaView,
     StyleSheet,
 } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
 import { COLORS } from '../constants/theme';
-import { getCategories, getTransactions } from '../redux/actions/';
 
 const SplashScreen = ({ navigation }) => {
 
-    const dispatch = useDispatch();
-    const { transactions } = useSelector(state => state.transactionsReducer);
-    const { categories } = useSelector(state => state.categoriesReducer);
-
-    useEffect(() => {
-        navigation.navigate('HomeScreen');
-    }, [transactions, categories])
-
-    const getData = () => {
-        // console.log(transactions, categories)
-        dispatch(getTransactions());
-        dispatch(getCategories());
-    }
-
     return (
         <SafeAreaView style={[styles.background, styles.alignCenter]}>
-            <Text>Loading...</Text>
-            {getData()}
+            <Text>Loading...</Text>s
         </SafeAreaView>
     )
 }
