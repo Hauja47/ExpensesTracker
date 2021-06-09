463648f07abe36e7040ffb33774c8927b19740fa
Hauja47 <hoangletrunghau01@gmail.com>
2021-06-09 09:19:03 +0700

    Add isTransactionsLoaded state, add addData function

--- src/redux/reducers/transactionsReducer.js
+++ src/redux/reducers/transactionsReducer.js
@@ -1,28 +1,36 @@
 import { ADD_TRANSACTIONS, GET_TRANSACTIONS } from '../actions/actions';
 
 const initialState = {
-    transactions: []
+    transactions: [],
+    isTransactionsLoaded: false
+}
+
+function addData (array, data) {
+    let dataSameDate = array.find(item => item.date == data.date);
+    if (dataSameDate) {
+        dataSameDate.data.push(data)
+    } else {
+        array.push({
+            date: data.date,
+            data: [data]
+        })
+    }
+
+    return array;
 }
 
 function categoriesReducer(state = initialState, action) {
     switch (action.type) {
         case ADD_TRANSACTIONS:
-            let found = transactions.find(element => element.date === action.payload.date);
-            if (found) {
-                return {
-                    ...state,
-                    transactions: found.data.push(action.payload),
-                };
-            } else {
-                return {
-                    ...state,
-                    transactions: [...state.transactions, action.payload],
-                };
+            return {
+                ...state,
+                transactions: addData(state.transactions, action.payload)
             }
         case GET_TRANSACTIONS:
             return {
                 ...state,
                 transactions: action.payload,
+                isTransactionsLoaded: true
             };
         default:
             return state;
