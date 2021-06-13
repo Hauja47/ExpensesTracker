36f3386e4a81dbb87a5b8f2d6a9a880b731d6a29
Hauja47 <hoangletrunghau01@gmail.com>
2021-06-13 10:24:38 +0700

    Update UI

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
     Text,
     SafeAreaView,
     StyleSheet,
+    View
 } from 'react-native';
-import { COLORS } from '../constants/theme';
+import { LinearProgress } from 'react-native-elements';
+import { COLORS, FONTS, SIZES } from '../constants/theme';
 
 const SplashScreen = () => {
     return (
-        <SafeAreaView style={[styles.background, styles.alignCenter]}>
-            <Text>Loading...</Text>
+        <SafeAreaView style={styles.background}>
+            <View style={styles.alignCenter}>
+                <Text style={styles.appName}>Expense Tracker</Text>
+                <LinearProgress
+                    color={COLORS.white}
+                    style={styles.loadBar}
+                />
+            </View>
         </SafeAreaView>
     )
 }
@@ -20,8 +28,17 @@ const styles = StyleSheet.create({
         backgroundColor: COLORS.blue
     },
     alignCenter: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    appName: {
+        ...FONTS.largeTitle,
+        color: COLORS.white
+    },
+    loadBar: {
+        width: SIZES.width - 20,
+        margin: 20
     }
 })
 
