2767aab368cd52e9006ebb8548e821ee35c041b9
Hauja47 <hoangletrunghau01@gmail.com>
2021-06-14 20:54:42 +0700

    Remove ViewAllUser

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,5 @@
 import HomeScreen from "./HomeScreen";
 import TransactionDetail from "./TransactionDetail"
-import ViewAllUser from "./ViewAllUser"
 import AddTransaction from "./AddTransaction";
 import AddCategory from './AddCategory'
 import SplashScreen from "./SplashScreen";
@@ -8,7 +7,6 @@ import SplashScreen from "./SplashScreen";
 export {
     HomeScreen,
     TransactionDetail,
-    ViewAllUser,
     AddTransaction,
     AddCategory,
     SplashScreen
